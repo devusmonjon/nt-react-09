031cd25163e293c8dc58f3c1be01abdf1259f02a
devusmonjon <usmonjonhasanov777@gmail.com>
2024-09-18 12:40:41 +0500

    fix

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,83 @@
+import { useSelector } from "react-redux";
+
 const Home = () => {
-  return <div>Home</div>;
+  const user = useSelector((state: any) => state.auth.user);
+  console.log(user);
+
+  return (
+    <section>
+      <div className="container mt-10">
+        <h2 className="scroll-m-20 border-b pb-2 mb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Profile
+        </h2>
+        <p className="text-muted-foreground">This is your profile page.</p>
+        <div className="my-6 w-full overflow-y-auto">
+          <table className="w-full">
+            <thead>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
+                  Key
+                </th>
+                <th className="border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right">
+                  value
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  #ID
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {user._id}
+                </td>
+              </tr>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  First Name
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {user.first_name}
+                </td>
+              </tr>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  email
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {user.email}
+                </td>
+              </tr>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  Created Time
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {new Date(user.createdAt).toLocaleString()}
+                </td>
+              </tr>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  Role
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {user.role}
+                </td>
+              </tr>
+              <tr className="m-0 border-t p-0 even:bg-muted">
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  Status
+                </td>
+                <td className="border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right">
+                  {user.status}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
